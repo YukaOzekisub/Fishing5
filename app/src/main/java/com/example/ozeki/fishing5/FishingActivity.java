cea2604e4452c4777562f00b9340c80caf526082
Ozeki Yuka <ozeki@OzekinoMacBook-Pro.local>
2018-02-19 15:47:53 +0900

    finish animation

--- app/src/main/java/com/example/ozeki/fishing5/FishingActivity.java
+++ app/src/main/java/com/example/ozeki/fishing5/FishingActivity.java
@@ -6,11 +6,15 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -19,6 +23,18 @@ public class FishingActivity extends AppCompatActivity {
     SharedPreferences pref;
     Bitmap target_location_frame;
     private FrameLayout.LayoutParams buttonLayoutParams;
+
+    double target_radius;
+    double target_location_pt_x;
+    double target_location_pt_y;
+
+    ImageView fishing_cat;
+    ImageView light;
+    ImageView turizao;
+
+    private TranslateAnimation translateAnimation;
+
+    ImageView location_frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +49,21 @@ public class FishingActivity extends AppCompatActivity {
             target_location_frame = BitmapFactory.decodeByteArray(b, 0, b.length).copy(Bitmap.Config.ARGB_8888, true);
         }
 
-        ImageView location_frame = findViewById(R.id.target_location_view);
+        fishing_cat = (ImageView) findViewById(R.id.fishing_cat);
+        fishing_cat.setVisibility(View.INVISIBLE);
+        light = (ImageView) findViewById(R.id.light);
+        light.setVisibility(View.INVISIBLE);
+        turizao = (ImageView) findViewById(R.id.turizao);
+        turizao.setVisibility(View.INVISIBLE);
+
+        location_frame = findViewById(R.id.target_location_view);
         location_frame.setImageBitmap(target_location_frame);
 
         ImageView target_location_button = findViewById(R.id.target_location_button);
         Intent intent = getIntent();
-        double target_radius = intent.getDoubleExtra("target_radius", 0);
-        double target_location_pt_x = intent.getDoubleExtra("target_location_pt_x", 0);
-        double target_location_pt_y = intent.getDoubleExtra("target_location_pt_y", 0);
+        target_radius = intent.getDoubleExtra("target_radius", 0);
+        target_location_pt_x = intent.getDoubleExtra("target_location_pt_x", 0);
+        target_location_pt_y = intent.getDoubleExtra("target_location_pt_y", 0);
 
         Log.d("ポイントx", ""+target_location_pt_x);
         Log.d("ポイントy", ""+target_location_pt_y);
@@ -60,5 +83,84 @@ public class FishingActivity extends AppCompatActivity {
         target_location_button.setLayoutParams(buttonLayoutParams);
         target_location_button.setVisibility(View.VISIBLE);
 
+        findViewById(R.id.start_fishing_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.start_fishing_button).setVisibility(View.INVISIBLE);
+                start_fishing(view);
+            }
+        });
+
+    }
+
+    private void start_fishing(View view){
+
+        //釣り始め
+        int fishing_cat_width = 250;
+        int fishing_cat_height = 250;
+        FrameLayout.LayoutParams fishingCatLayoutParams = new FrameLayout.LayoutParams(fishing_cat_width, fishing_cat_height);
+
+        fishingCatLayoutParams.setMargins(
+                (int)(location_frame.getPaddingLeft() + target_location_pt_x + target_radius + fishing_cat_width),
+                (int)(location_frame.getPaddingTop() + target_location_pt_y - fishing_cat_height),
+                fishingCatLayoutParams.rightMargin,
+                fishingCatLayoutParams.bottomMargin
+        );
+
+        fishing_cat.setImageResource(R.drawable.neko_sit_gray);
+        fishing_cat.setLayoutParams(fishingCatLayoutParams);
+        fishing_cat.setVisibility(View.VISIBLE);
+
+        //釣り始め
+        int turizao_width = 120;
+        int turizao_height = 180;
+        FrameLayout.LayoutParams turizaoLayoutParams = new FrameLayout.LayoutParams(turizao_width, turizao_height);
+
+        turizaoLayoutParams.setMargins(
+                (int)(location_frame.getPaddingLeft() + target_location_pt_x + target_radius + (fishing_cat_width/2)),
+                (int)(location_frame.getPaddingTop() + target_location_pt_y - (fishing_cat_height/2)),
+                fishingCatLayoutParams.rightMargin,
+                fishingCatLayoutParams.bottomMargin
+        );
+
+
+        turizao.setLayoutParams(turizaoLayoutParams);
+        turizao.setVisibility(View.VISIBLE);
+
+        //釣竿のアニメーション
+        turizao.setBackground(ContextCompat.getDrawable(FishingActivity.this, R.drawable.turizao_animation));
+        // AnimationDrawableを取得
+        AnimationDrawable turizaoAnimation = (AnimationDrawable) turizao.getBackground();
+        // アニメーションの開始
+        turizaoAnimation.start();
+
+        //電球つける
+        int light_width = 80;
+        int light_height = 80;
+        FrameLayout.LayoutParams lightLayoutParams = new FrameLayout.LayoutParams(light_width, light_height);
+
+        lightLayoutParams.setMargins(
+                (int)(location_frame.getPaddingLeft() + target_location_pt_x + target_radius + fishing_cat_width),
+                (int)(location_frame.getPaddingTop() + target_location_pt_y - fishing_cat_height),
+                fishingCatLayoutParams.rightMargin,
+                fishingCatLayoutParams.bottomMargin
+        );
+
+
+        light.setLayoutParams(lightLayoutParams);
+        light.setVisibility(View.VISIBLE);
+
+        //ライトのアニメーション
+        light.setBackground(ContextCompat.getDrawable(FishingActivity.this, R.drawable.light_animation));
+        // AnimationDrawableを取得
+        AnimationDrawable lightAnimation = (AnimationDrawable) turizao.getBackground();
+        // アニメーションの開始
+        lightAnimation.start();
+/*
+        //画面遷移
+        Intent intent = new Intent(FishingActivity.this, ResultActivity.class);
+        intent.putExtra("target_radius", target_radius);
+        startActivity(intent);
+*/
     }
 }
---
+++ app/src/main/java/com/example/ozeki/fishing5/ResultActivity.java
@@ -0,0 +1,83 @@
+package com.example.ozeki.fishing5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class ResultActivity extends AppCompatActivity {
+
+    double target_radius;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        target_radius = intent.getDoubleExtra("target_radius", 0);
+
+        int item_list[] = new int[15];
+        String item_text_list[] = new String[15];
+
+        item_list[0] = R.drawable.coffee; item_text_list[0] = "コーヒー が釣れた！";
+        item_list[1] = R.drawable.onigiri;item_text_list[1] = "おにぎり が釣れた！";
+        item_list[2] = R.drawable.hotdog;item_text_list[2] = "ホットドッグ が釣れた！";
+
+
+        item_list[3] = R.drawable.beer;item_text_list[3] = "ビール が釣れた！";
+        item_list[4] = R.drawable.hotdog;item_text_list[4] = "ホットドッグ が釣れた！";
+        item_list[5] = R.drawable.kasa;item_text_list[5] = "傘 が釣れた！";
+        item_list[6] = R.drawable.kingyo;item_text_list[6] = "金魚 が釣れた！";
+        item_list[7] = R.drawable.tako;item_text_list[7] = "タコ が釣れた！";
+        item_list[8] = R.drawable.ika;item_text_list[8] = "イカ が釣れた！";
+        item_list[9] = R.drawable.fish1;item_text_list[9] = "魚 が釣れた！";
+        item_list[10] = R.drawable.bousi;item_text_list[10] = "帽子 が釣れた！";
+
+        item_list[11] = R.drawable.kinko;item_text_list[11] = "金庫 が釣れた！";
+        item_list[12] = R.drawable.fish3;item_text_list[12] = "魚 が釣れた！";
+        item_list[13] = R.drawable.fish2;item_text_list[13] = "魚 が釣れた！";
+        item_list[14] = R.drawable.fish1;item_text_list[14] = "魚 が釣れた！";
+        item_list[15] = R.drawable.controller;item_text_list[15] = "コントローラー が釣れた！";
+
+        ImageView result_item = (ImageView) findViewById(R.id.resultItem);
+        TextView result_item_text = (TextView) findViewById(R.id.itemText);
+        int item_num = get_item_num();
+        result_item.setImageResource(item_list[item_num]);
+        result_item_text.setText(item_text_list[item_num]);
+    }
+
+    private int get_item_num(){
+
+        //画面サイズの取得
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        Display disp = wm.getDefaultDisplay();
+        android.graphics.Point realSize = new android.graphics.Point();
+        disp.getRealSize(realSize);
+
+        int screen_width = realSize.x;
+        int screen_height = realSize.y;
+
+        int randomNumber = 0;
+
+        if(target_radius < screen_height / 9){
+            Random rand = new Random();
+            randomNumber = rand.nextInt(3);
+            return randomNumber;
+        }
+
+        if(target_radius < screen_height / 6) {
+            Random rand = new Random();
+            randomNumber = rand.nextInt(8);
+            return (randomNumber + 3);
+        }
+        Random rand = new Random();
+        randomNumber = rand.nextInt(5);
+        return (randomNumber + 11);
+    }
+}
